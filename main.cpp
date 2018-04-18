8fac1769090538f482f8af72243c485ccc2aee58
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-04-17 23:50:10 -0700

    greatest to least

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void displaySort (int&, int&, int&);
+
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -9,8 +11,8 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
-
-
+  
+  displaySort(red, green, blue);
 
 
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -19,3 +21,37 @@ int main()
   return 0;
   //...END OF "DO NOT CHANGE" AREA
 }
+
+
+void displaySort (int&val1, int&val2, int&val3)
+  { 
+    if (val2 > val1)
+      {
+        int tmp = val1;
+        val1 = val2;
+        val2 = tmp;
+      }
+
+    if (val3>val2)
+      {
+        int tmp = val2;
+        val2 = val3;
+        val3 =tmp;
+      }
+
+   if (val2 > val1)
+     {
+       int tmp = val1;
+       val1 = val2;
+       val2 = tmp;
+     }
+ }
+
+
+
+
+
+
+
+
+
